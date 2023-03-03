11a49dde45df180797f63727e60ae8f37b5e186b
00zy <1605482347@qq.com>
2023-03-03 22:17:16 +0800

    更新了一些路由设置

--- OnlineExamSystem/urls.py
+++ OnlineExamSystem/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-# 主路由
+# 主路由：整个项目的启动路径
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # url(r'^ckeditor/', include('ckeditor_uploader.urls')),  # 导入图片操作
+    # path('', include('front.urls')),  # 包含了整个项目的路由
+    # 没有这一句无法显示上传的图片
+]  # + static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
